3015288ffb4c633f60551d7c2b645b37e2f26552
ru <rubygaming6@gmail.com>
2025-05-11 04:44:41 -0400

    added to stats. main implementation nearly finished

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from stats import count_words, count_chars, sort_dict
+
+def sort_on(dict):
+    return dict["num"]
+
 def get_book_text(filepath):
     """
     Args:
@@ -10,10 +15,28 @@ def get_book_text(filepath):
         return f.read()
 
 def main():
-    print(get_book_text("./books/frankenstein.txt"))
+    print("============ BOOKBOT ============")
+
+    text = get_book_text("./books/frankenstein.txt")
+
+    print("Analyzing book found at books/frankenstein.txt")
+    print("----------- Word Count ----------")
+
+    count_words(text)
+
+    print("--------- Character Count -------")
+    char_dict = count_chars(text) 
+
+    sorted_char_list = sort_dict(char_dict)
+
+    for char in sorted_char_list:
+        print(f"{char['char']}: {char['num']}")
+
+    print("============= END ===============")
 
 
 
 
-main()    
+
+main()
 
--- stats.py
+++ stats.py
@@ -1,3 +1,6 @@
+def sort_on(dict):
+    return dict["num"]
+
 def count_words(book_text):
     """
     Args:
@@ -7,5 +10,46 @@ def count_words(book_text):
     """
     word_list = book_text.split()
     num_words = len(word_list)
-    print(f"{num_words} words found in the document")
+    print(f"Found {num_words} total words")
     return num_words
+
+def count_chars(book_text):
+    """
+    Args:
+        book_text (str): text from book
+    Returns:
+        dict: char-->count 
+    """
+    chars = {}
+
+
+    for c in book_text:
+        c = c.lower()
+        if c in chars:
+            chars[c] += 1
+        else:
+            chars[c] = 1
+    return chars
+
+def sort_dict(char_dict):
+    """
+    Args:
+        dict: char-->int to be sorted
+    Returns:
+        list of dicts: e.g. {"char": "b", "num": 4868}
+    """
+    char_list = []
+    for char in char_dict:
+        if char.isalpha():
+            key = char
+            val = char_dict[char]
+            char_new = {"char": f"{key}", "num": val}
+            char_list.append(char_new)
+
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
+
+
+
+
+
